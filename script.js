1c9ae4907ae939b8689dc25c5c3e2e2d64ac1d5b
Adarsh Abhishek <adarshabhishek1203@gmail.com>
2025-06-01 13:04:52 +0530

    Update project with recent changes

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const themeToggle = document.querySelector(".theme-toggle");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = document.querySelector(".prompt-input");
 const promptBtn = document.querySelector(".prompt-btn");
-const modelSelect = document.querySelector(".model-select");
-const countSelect = document.querySelector(".count-select");
-const ratioSelect = document.querySelector(".ratio-select");
+const modelSelect = document.getElementById("model-select");
+const countSelect = document.getElementById("count-select");
+const ratioSelect = document.getElementById("ratio-select");
 const gridGallery = document.querySelector(".gallery-grid");
 
 const examplePrompts = [
