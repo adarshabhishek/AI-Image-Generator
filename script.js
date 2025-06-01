c9d9d93cef9eca3fdb74f82cd73c9df7defd4e43
Adarsh Abhishek <adarshabhishek1203@gmail.com>
2025-06-01 16:04:39 +0530

    API integration

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const themeToggle = document.querySelector(".theme-toggle");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = document.querySelector(".prompt-input");
 const promptBtn = document.querySelector(".prompt-btn");
+const generateBtn = document.querySelector(".generate-btn");
 const modelSelect = document.getElementById("model-select");
 const countSelect = document.getElementById("count-select");
 const ratioSelect = document.getElementById("ratio-select");
 const gridGallery = document.querySelector(".gallery-grid");
+
+const API_KEY = "ADD_YOUR_API_KEY_HERE";
 
 const examplePrompts = [
   "A magic forest with glowing plants and fairy homes among giant mushrooms",
@@ -41,6 +44,74 @@ const toggleTheme = () => {
     themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon"
 };
 
+const getImageDimensions = (aspectRatio, baseSize=512) =>{
+    const [width, height]= aspectRatio.split("/").map(Number);
+    const scaleFactor = baseSize / Math.sqrt(width * height);
+
+    let calculatedWidth = Math.round(width*scaleFactor);
+    let calculatedHeight = Math.round(height*scaleFactor);
+
+    calculatedWidth= Math.floor(calculatedWidth/16) * 16;
+    calculatedHeight= Math.floor(calculatedHeight/16) * 16;
+
+    return {width: calculatedWidth, height: calculatedHeight};
+};
+
+const updateImageCard = (imgIndex, imgUrl) => {
+    const imgCard =document.getElementById(`img-card-${imgIndex}`);
+    if(!imgCard) return;
+
+    imgCard.classList.remove("loading");
+    imgCard.innerHTML = `<img src="${imgUrl}" class="result-img"/>
+                        <div class="img-overlay">
+                            <a href="${imgUrl}" class="img-download-btn" download="${Date.now()}.png">
+                                <i class="fa-solid fa-download"></i>
+                            </a>
+                        </div>`;
+}
+
+
+
+
+
+
+
+const generateImages = async (selectedModel, imageCount, aspectRatio, promptText) => {
+    const MODEL_URL = `https://router.huggingface.co/hf-inference/models/${selectedModel}`;
+    const {width,height} = getImageDimensions(aspectRatio);
+    generateBtn.setAttribute("disabled", "true");
+
+    const imagePromises = Array.from({length: imageCount}, async(_, i) => {
+        try{
+        const response = await fetch(MODEL_URL, {
+          headers: {
+				Authorization: `Bearer ${API_KEY}`,
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify({
+                inputs :promptText,
+                parameters: {width, height},
+                options: {wait_for_model: true, user_cache: false},
+            }),  
+        });
+
+        if (!response.ok) throw new Error((await response.json())?.error);
+
+        const result = await response.blob();
+        updateImageCard(i, URL.createObjectURL(result));
+
+    } catch{
+        console.log(error);
+        const imgCard = document.getElementById(`img-card-${i}`);
+        imgCard.classList.replace("loading", "error");
+        imgCard.querySelector(".status-text").textContent = "Generation failed!";
+    }
+    })
+   await Promise.allSettled(imagePromises); 
+   generateBtn.removeAttribute("disabled", "false");
+};
+
 const createImageCards = (selectedModel, imageCount, aspectRatio, promptText) => {
     gridGallery.innerHTML = "";
 
@@ -51,9 +122,9 @@ const createImageCards = (selectedModel, imageCount, aspectRatio, promptText) =>
                             <i class="fa-solid fa-triangle-exclamation"></i>
                             <p class="status-text">Generating...</p>
                         </div>
-                        <img src="test.avif" class="result-img"/>
                     </div>`;
     }
+    generateImages(selectedModel, imageCount, aspectRatio, promptText)
 }
 
 const handleFormSubmit = (e) => {
